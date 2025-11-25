481657692fd4c9857f1cca2b599a101b6a440eff
ahmedfathi <ahmedfathi1792@yahoo.com>
2025-11-25 18:51:12 +0200

    geniral modification

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,24 +12,24 @@ export const metadata: Metadata = {
   title: "Portfolio - Creative Developer",
   description:
     "Showcasing my web development projects and skills. Specializing in React, Next.js, and full-stack development.",
-  generator: "v0.app",
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
+  generator: "Ahmed",
+  // icons: {
+  //   icon: [
+  //     {
+  //       url: "/icon-light-32x32.png",
+  //       media: "(prefers-color-scheme: light)",
+  //     },
+  //     {
+  //       url: "/icon-dark-32x32.png",
+  //       media: "(prefers-color-scheme: dark)",
+  //     },
+  //     {
+  //       url: "/icon.svg",
+  //       type: "image/svg+xml",
+  //     },
+  //   ],
+  //   apple: "/apple-icon.png",
+  // },
 }
 
 export default function RootLayout({
